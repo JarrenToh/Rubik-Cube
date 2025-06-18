aa836b97cf5bdefbb9ad6b7bc30f5b7231cadbf8
Tan <mavric.tan93@gmail.com>
2025-06-18 21:45:11 +0800

    fix orange color code

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ export default function App() {
     white: new THREE.Color().setHex(0xffffff),
     yellow: new THREE.Color().setHex(0xffff00),
     red: new THREE.Color().setHex(0xff0000),
-    orange: new THREE.Color().setHex(0xffa07a),
+    orange: new THREE.Color().setHex(0xffa500),
     blue: new THREE.Color().setHex(0x0000ff),
     green: new THREE.Color().setHex(0x008000),
     black: new THREE.Color().setHex(0x000000),
